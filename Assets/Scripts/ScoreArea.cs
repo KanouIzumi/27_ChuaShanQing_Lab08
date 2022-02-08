74c72255cce4f1ba693937c1e96f68536ed9ba29
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2022-02-08 16:39:13 +0800

    I have added some sound effects and bgms

--- Assets/Scripts/ScoreArea.cs
+++ Assets/Scripts/ScoreArea.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class ScoreArea : MonoBehaviour
 {
+
+    private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip m_GetScore;           // the sound played when character touches the coin.
+
+
     public Text scoreText;
     int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,13 +25,21 @@ public class ScoreArea : MonoBehaviour
         
     }
 
-     void OnTriggerEnter(Collider collision)
+
+    private void PlayGetScoresound()
+    {
+        m_AudioSource.clip = m_GetScore;
+        m_AudioSource.Play();
+    }
+
+    void OnTriggerEnter(Collider collision)
     {
 
 
         if (collision.gameObject.tag == "Obstacle")
         {
             score++;
+            PlayGetScoresound();
             scoreText.text = "Score: " + score;
             Destroy(collision.gameObject);
         }
--- Assets/Scripts/Spawnerscript.cs
+++ Assets/Scripts/Spawnerscript.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
+
+    private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip m_ObstacleSpawn;           // the sound played something is spawn
+
+
     public GameObject[] SpawnObject;
     float PositionY;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_AudioSource = GetComponent<AudioSource>();
+
         InvokeRepeating("SpawnObjects", 1, 1);
     }
 
@@ -21,12 +28,20 @@ public class Spawnerscript : MonoBehaviour
 
     }
 
+
+    private void PlayObstacleSpawn()
+    {
+        m_AudioSource.clip = m_ObstacleSpawn;
+        m_AudioSource.Play();
+    }
+
     void SpawnObjects()
     {
         int randomIndex = Random.Range(0, SpawnObject.Length);
 
         PositionY = Random.Range(4, -4f);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        PlayObstacleSpawn();
         Instantiate(SpawnObject[randomIndex], transform.position, transform.rotation);
     }
 }
